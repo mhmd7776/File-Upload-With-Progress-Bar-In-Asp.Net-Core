e86a65292ba38053053a00d976b8d5a21dc0a9af
mhmd7776 <mhmd7776@yahoo.com>
2022-11-01 20:42:43 +0330

    Add project files.

---
+++ FileUploadWithProgressBar/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadWithProgressBar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static int Progress { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+
+            var buffer = new byte[16 * 1024];
+
+            await using (var output = System.IO.File.Create(GetFullPath(fileName)))
+            {
+                await using (var input = file.OpenReadStream())
+                {
+                    var totalReadBytes = 0;
+                    int readBytes;
+
+                    while ((readBytes = await input.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        await output.WriteAsync(buffer, 0, readBytes);
+                        totalReadBytes += readBytes;
+                        Progress = (int)(totalReadBytes / (float)file.Length * 100.0);
+                        await Task.Delay(10);
+                    }
+                }
+            }
+
+            return Ok(fileName);
+        }
+
+        public string GetFullPath(string fileName)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory() + "/wwwroot/files/");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            return path + fileName;
+        }
+
+        [HttpPost]
+        public IActionResult GetProgress()
+        {
+            return Ok(Progress.ToString());
+        }
+    }
+}
---
+++ FileUploadWithProgressBar/wwwroot/js/site.js
@@ -0,0 +1,48 @@
+﻿var fileInput = document.getElementById("FileInput");
+var UploadProgressBox = document.getElementById("UploadProgressBox");
+var UploadProgressBar = document.getElementById("UploadProgressBar");
+var Interval;
+
+fileInput.addEventListener("change", function (event) {
+    // files validation
+    const files = fileInput.files;
+    if (!files.length) {
+        return;
+    }
+
+    // create and add file to formData
+    const formData = new FormData();
+    formData.append("file", files[0]);
+
+    // show progress bar
+    UploadProgressBar.classList.remove("bg-success");
+    UploadProgressBar.classList.add("bg-primary");
+    UploadProgressBox.classList.remove("d-none");
+
+    // set interval and send ajax to get current progress value 
+    Interval = setInterval(
+        function () {
+            $.post("/Home/GetProgress", function (progress) {
+                UploadProgressBar.innerText = progress + "%";
+                UploadProgressBar.style.width = progress + "%";
+            });
+        },
+        10
+    );
+
+    // send file with ajax
+    $.ajax({
+        url: "/Home/UploadFile",
+        data: formData,
+        processData: false,
+        contentType: false,
+        type: "POST",
+        success: function (fileName) {
+            clearInterval(Interval);
+            UploadProgressBar.classList.remove("bg-primary");
+            UploadProgressBar.classList.add("bg-success");
+            console.info(`File Uploaded Successfully => filename : ${fileName}`);
+        }
+    });
+});
+
